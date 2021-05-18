a484a72bc8b7bf04ab1dddd37ba342818fc96023
Alexander Almström <info@alexanderalmstrom.com>
2021-05-18 16:35:09 +0200

    Remove react query dev tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "@styles/index.scss";
 import { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,7 +14,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
